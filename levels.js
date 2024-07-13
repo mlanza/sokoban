cd6a2c7430876ad49848027898b05125686f4219
Mario T. Lanza <me@mlanza.com>
2024-07-13 15:17:16 -0400

    add level

--- levels.js
+++ levels.js
@@ -6,6 +6,23 @@ export const _ = "void";
 export const levels = [];
 
 levels.push({
+  worker: [9,1],
+  crates: [[3,7],[10,7],[12,4]],
+  fixtures: [
+    [b, b, b, b, _, _, _, _, b, b, b, _, _, _, _],
+    [b, x, g, b, b, b, _, _, b, g, b, _, _, _, _],
+    [b, x, g, g, g, b, b, b, b, g, b, b, b, b, _],
+    [b, x, g, g, g, g, g, b, b, g, b, g, g, b, _],
+    [b, b, b, g, b, g, g, g, g, g, g, g, g, b, _],
+    [_, _, b, g, g, g, b, g, b, b, b, b, g, b, b],
+    [_, b, b, g, b, g, b, g, g, g, g, g, g, g, b],
+    [_, b, g, g, b, g, b, g, b, g, g, g, g, g, b],
+    [_, b, g, g, g, g, g, g, g, g, g, g, b, b, b],
+    [_, b, b, b, b, b, b, b, b, b, b, b, b, _, _]
+  ]
+});
+
+levels.push({
   worker: [2,6],
   crates: [[2,2],[4,2],[3,3],[2,5],[3,5]],
   fixtures: [
